040325e106d64197646cda635e534b4d10b61993
Godsplan Elot <elotgodsplan2@gmail.com>
2025-09-21 08:34:14 +0100

    Install Bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+//Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//  custom CSS 
+import './styles/custom.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
