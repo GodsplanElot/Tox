16208d77966b3dc8b53a9c6c4ec3dda361a8f65a
Godsplan Elot <elotgodsplan2@gmail.com>
2025-09-21 09:41:44 +0100

    Create basic project page structure

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// src/pages/Home.tsx
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="container py-5">
+      <h1>Welcome to MyMovieApp</h1>
+      <p>This is the homepage. Movies will appear here later.</p>
+    </div>
+  );
+};
+
+export default Home;
