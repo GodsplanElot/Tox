0f5fcd0086b864d7addefd03693ff0514868a177
GodsplanElot <elotgodsplan2@gmail.com>
2025-11-03 23:42:04 +0100

    clean up home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
   return (
     <div className="container py-5">
       <h1>Welcome to MyMovieApp</h1>
-      <p>This is the homepage. Movies will appear here later.</p>
+      <p>This is the homepage.  will appear here later.</p>
     </div>
   );
 };
