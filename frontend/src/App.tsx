28effb3d9151aba32e713f8579e774f0b5b1b410
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-02 08:45:49 +0100

    install react-bootstrap package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <div className="container py-5">
-      <h1 className="text-center">Fresh React + Bootstrap Start</h1>
-    </div>
-  )
+import AppRoutes from "./routes/AppRoutes"
+
+const App = () => {
+  return <AppRoutes />
 }
 
 export default App
