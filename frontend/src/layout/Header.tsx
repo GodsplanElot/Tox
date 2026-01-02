c9d4a6e7afe86ee4b31d35ba913ad13117026156
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-02 08:58:50 +0100

    modify header component (create hambuger at mobile break point)

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,10 +1,19 @@
-import { Navbar, Container } from "react-bootstrap"
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">MyApp</Navbar.Brand>
+        <Navbar.Brand href="/">TOX</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="main-navbar" />
+
+        <Navbar.Collapse id="main-navbar">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/movies">Movies</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
