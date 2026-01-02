4e9840ebcb01bb57a52777f134ff06fc3928b099
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-02 08:55:21 +0100

    modify main.tsx (take out old imports)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
 import App from './App'
 
-// Bootstrap CSS
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
-// Optional: minimal global resets (can even remove later)
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
