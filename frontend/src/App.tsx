a415e8ca947bb745f17c01facf059a5dcff106e3
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-02 06:45:38 +0100

    modify web app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,9 @@
-// src/App.tsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"; 
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Movies from "./pages/Movies";
-import TvSeries from "./pages/TvSeries";
-import Animations from "./pages/Animations";
-import AnimeSeries from "./pages/AnimeSeries";
-import MovieDetailPage from "./pages/MovieDetailPage";
-import MainLayout from "./layout/MainLayout";
-
-const App: React.FC = () => {
+function App() {
   return (
-    <MainLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/tv-series" element={<TvSeries />} />
-        <Route path="/animations" element={<Animations />} />
-        <Route path="/anime-series" element={<AnimeSeries />} />
-        <Route path="/movie/:id" element={<MovieDetailPage />} />
-      </Routes>
-    </MainLayout>
-  );
-};
+    <div className="container py-5">
+      <h1 className="text-center">Fresh React + Bootstrap Start</h1>
+    </div>
+  )
+}
 
-export default App;
+export default App
