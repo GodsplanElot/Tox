5fc5717e5d78c29d55a697bd7604b0ca770b948c
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-04 10:20:24 +0100

    modify home page to display movie rail

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,41 @@
 import HeroCarousel from "../components/HeroCarousel"
+import MovieRail from "../components/MovieRail"
+
+const mockMovies = [
+  {
+    id: "1",
+    title: "Endless Horizon",
+    poster: "/img/covers/cover1.jpg",
+    rating: 8.2,
+  },
+  {
+    id: "2",
+    title: "Dark Waters",
+    poster: "/img/covers/cover2.jpg",
+    rating: 7.9,
+  },
+  {
+    id: "3",
+    title: "Lost Signal",
+    poster: "/img/covers/cover3.jpg",
+    rating: 8.5,
+  },
+  {
+    id: "4",
+    title: "Neon City",
+    poster: "/img/covers/cover4.jpg",
+    rating: 7.4,
+  },
+]
 
 const Home = () => {
   return (
-    <div>
+    <>
       <HeroCarousel />
-    </div>
+
+      <MovieRail title="Trending Now" movies={mockMovies} />
+      <MovieRail title="Popular Movies" movies={mockMovies} />
+    </>
   )
 }
 
