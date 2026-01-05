4697cf668ecb8d3d867774dbc03f70c9f18bb1da
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-05 15:00:17 +0100

    add category route to AppRoutes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import Layout from "../layout/Layout"
 import Home from "../pages/Home"
 import NotFound from "../pages/NotFound"
+import Categories from "../pages/Categories"
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
