800379ff445ae64577374bf40e6cd1e1ea3754e4
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-06 10:05:38 +0100

    modify mock db

--- frontend/src/data/movies.ts
+++ frontend/src/data/movies.ts
@@ -66,7 +66,6 @@ export const moviesFromDb: Movie[] = [
     rating: 8.3,
     releaseDate: "2024-01-12",
     categoryIds: [1],
-  
     runtime: 125,
     genres: ["Action", "Sci-Fi"],
     description: "In a sprawling metropolis bathed in neon lights, a renegade detective uncovers a conspiracy that threatens to unravel the fabric of society.",
@@ -79,6 +78,10 @@ export const moviesFromDb: Movie[] = [
     rating: 7.6,
     releaseDate: "2022-08-03",
     categoryIds: [2],
+    runtime: 125,
+    genres: ["Action", "porn"],
+    description: "The story of a mute artist who finds his voice through a series of extraordinary events that challenge his perception of reality.",
+    trailer: "https://www.youtube.com/watch?v=neoncitytrailer"
   },
   {
     id: 103,
@@ -87,14 +90,21 @@ export const moviesFromDb: Movie[] = [
     rating: 7.9,
     releaseDate: "2023-06-21",
     categoryIds: [3],
+    runtime: 95,
+    genres: ["Comedy"],
+    description: "A group of misfit friends embark on a hilarious road trip that tests their friendship and leads to unexpected adventures.",
+    trailer: "https://www.youtube.com/watch?v=neoncitytrailer"
   },
   {
     id: 104,
-    title: "Laugh Rioyt",
+    title: "Riot 2",
     poster: "/img/covers/cover4.jpg",
     rating: 7.9,
     releaseDate: "2023-06-21",
     categoryIds: [3],
+    runtime: 100,
+    genres: ["action", "comedy"],
+    description: "Things go badly when the government tires to control the comedy industry.",
   },
   {
     id: 105,
@@ -103,6 +113,9 @@ export const moviesFromDb: Movie[] = [
     rating: 8.1,
     releaseDate: "2023-06-21",
     categoryIds: [3],
+    runtime: 110,
+    genres: ["Drama", "Mystery"],
+    description: "In a small town shrouded in secrets, a young detective uncovers dark truths that have been buried for decades.",
   },
   
 ];
