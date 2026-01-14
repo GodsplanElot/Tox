b0e9c1e8dfc850317b3e6730ae2de1a700eee61b
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-14 09:54:31 +0100

    Import mock DB to home page and modify home page to display series

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import HeroCarousel from "../components/HeroCarousel";
 import MovieRail from "../components/MovieRail/MovieRail";
 import MovieGrid from "../components/MovieGrid/MovieGrid";
+import SeriesRail from "../components/SeriesRail/SeriesRail"
 import { moviesFromDb, mockMovies } from "../data/movies";
+import { seriesFromDb } from "../data/series"
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
       <HeroCarousel />
 
       <MovieRail title="Trending Now" movies={mockMovies} />
+      <SeriesRail title="Trending Series" series={seriesFromDb} />
       <MovieRail title="Popular Movies" movies={mockMovies} />
 
       <MovieGrid title="All Movies" movies={moviesFromDb} />
