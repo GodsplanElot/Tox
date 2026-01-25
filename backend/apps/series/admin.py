d7d5ed110a7bfdfdf82ec51c3cb5491e4326ce02
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-25 13:50:27 +0100

    feat: Register series and media models with Django admin.

--- backend/apps/series/admin.py
+++ backend/apps/series/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Series
+from .models import Season
+from .models import Episode
+from .models import EpisodeVideo
+
+admin.site.register(Series)
+admin.site.register(Season)
+admin.site.register(Episode)
+admin.site.register(EpisodeVideo)
