486c8b6442f5080e789157a9a96adfaac37491e1
GodsplanElot <elotgodsplan2@gmail.com>
2026-01-25 11:37:58 +0100

    Modify movie model to implement upload logic and url logic

--- backend/apps/movies/models.py
+++ backend/apps/movies/models.py
@@ -1,3 +1,38 @@
+# apps/movies/models.py
 from django.db import models
+from apps.categories.models import Category
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    poster = models.URLField()  # single image
+
+    rating = models.FloatField(null=True, blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    runtime = models.PositiveIntegerField(null=True, blank=True)
+
+    # VIDEO / DOWNLOAD OPTIONS
+    video_file = models.FileField(
+        upload_to="movies/videos/",
+        blank=True,
+        null=True
+    )
+
+    external_download_url = models.URLField(
+        blank=True,
+        null=True,
+        help_text="External download or streaming link"
+    )
+
+    categories = models.ManyToManyField(
+        Category,
+        related_name="movies",
+        blank=True
+    )
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
